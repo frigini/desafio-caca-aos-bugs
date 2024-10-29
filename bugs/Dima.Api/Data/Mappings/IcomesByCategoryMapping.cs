a4775d6cee51be28d27e75ecfa29330e1aa8b185
Filipe Frigini <filipe.frigini@gmail.com>
2024-10-28 22:15:11 -0300

    corrige bugs do desafio

---
+++ bugs/Dima.Api/Data/Mappings/IcomesByCategoryMapping.cs
@@ -0,0 +1,23 @@
+﻿using Dima.Core.Models.Reports;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dima.Api.Data.Mappings
+{
+    public class IcomesByCategoryMapping : IEntityTypeConfiguration<IncomesByCategory>
+    {
+        public void Configure(EntityTypeBuilder<IncomesByCategory> builder)
+        {
+            builder.ToView("vwGetIncomesByCategory");
+            builder.HasNoKey();
+            builder
+                .Property(x => x.Incomes)
+                .HasPrecision(18, 2);
+
+            builder.Property(x => x.UserId);
+            builder.Property(x => x.Year);
+            builder.Property(x => x.Category);
+        }
+    }
+
+}
---
+++ bugs/Dima.Api/Data/Mappings/IncomesAndExpensesMapping.cs
@@ -0,0 +1,26 @@
+﻿using Dima.Core.Models.Reports;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dima.Api.Data.Mappings
+{
+    public class IncomesAndExpensesMapping : IEntityTypeConfiguration<IncomesAndExpenses>
+    {
+        public void Configure(EntityTypeBuilder<IncomesAndExpenses> builder)
+        {
+            builder.ToView("vwGetIncomesAndExpenses");
+            builder.HasNoKey();
+            builder
+                .Property(x => x.Incomes)
+                .HasPrecision(18, 2);
+
+            builder
+               .Property(x => x.Expenses)
+               .HasPrecision(18, 2);
+
+            builder.Property(x => x.UserId);
+            builder.Property(x => x.Month);
+            builder.Property(x => x.Year);
+        }
+    }
+}
