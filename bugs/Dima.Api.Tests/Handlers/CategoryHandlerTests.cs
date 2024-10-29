aead4fff6f7efd1c7ab269916da9535ff0a14fbc
Filipe Frigini <filipe.frigini@gmail.com>
2024-10-28 22:15:28 -0300

    cria projeto de teste para os handlers

---
+++ bugs/Dima.Api.Tests/Handlers/CategoryHandlerTests.cs
@@ -0,0 +1,228 @@
+﻿using Dima.Api.Data;
+using Dima.Api.Handlers;
+using Dima.Core.Models;
+using Dima.Core.Requests.Categories;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dima.Api.Tests.Handlers
+{
+    public class CategoryHandlerTests
+    {
+        private readonly DbContextOptions<AppDbContext> _options;
+        private readonly AppDbContext _appDbContext;
+        private readonly CategoryHandler _handler;
+
+        public CategoryHandlerTests()
+        {
+            _options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            _appDbContext = new AppDbContext(_options);
+            _handler = new CategoryHandler(_appDbContext);
+        }
+
+        [Fact]
+        public async Task CreateAsync_ValidRequest_ReturnSuccessResponse()
+        {
+            // Arrange
+            var createCategoryRequest = new CreateCategoryRequest
+            {
+                UserId = Guid.NewGuid().ToString(),
+                Title = "Category Test",
+                Description = "Description Test"
+            };
+
+            // Act
+            var result = await _handler.CreateAsync(createCategoryRequest);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(result.IsSuccess);
+            Assert.NotNull(result.Data);
+            Assert.Equal(createCategoryRequest.UserId, result.Data.UserId);
+            Assert.Equal(createCategoryRequest.Title, result.Data.Title);
+            Assert.Equal(createCategoryRequest.Description, result.Data.Description);
+        }
+
+        [Fact]
+        public async Task UpdateAsync_CategoryNotExists_ReturnsNotFoundResponse()
+        {
+            // Arrange
+            var updateCategoryRequest = new UpdateCategoryRequest
+            {
+                Id = 10000,
+                UserId = Guid.NewGuid().ToString(),
+                Title = "Category Test",
+                Description = "Description Test"
+            };
+
+            // Act
+            var result = await _handler.UpdateAsync(updateCategoryRequest);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Null(result.Data);
+            Assert.False(result.IsSuccess);
+        }
+
+        [Fact]
+        public async Task UpdateAsync_CategoryExists_ReturnsSuccessResponse()
+        {
+            // Arrange
+            var userId = Guid.NewGuid().ToString();
+            var category = new Category
+            {
+                UserId = userId,
+                Title = "Title original",
+                Description = "Description original"
+            };
+
+            await _appDbContext.Categories.AddAsync(category);
+            await _appDbContext.SaveChangesAsync();
+
+            var updateCategoryRequest = new UpdateCategoryRequest
+            {
+                Id = category.Id,
+                UserId = userId,
+                Title = "New Title",
+                Description = "New Description"
+            };
+
+            // Act
+            var result = await _handler.UpdateAsync(updateCategoryRequest);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(result.IsSuccess);
+            Assert.NotNull(result.Data);
+            Assert.Equal(updateCategoryRequest.Title, result.Data.Title);
+            Assert.Equal(updateCategoryRequest.Description, result.Data.Description);
+        }
+
+        [Fact]
+        public async Task DeleteAsync_CategoryNotExists_ReturnsNotFoundResponse()
+        {
+            // Arrange
+            var deleteCategoryRequest = new DeleteCategoryRequest
+            {
+                Id = 10000,
+                UserId = Guid.NewGuid().ToString()
+            };
+
+            // Act
+            var result = await _handler.DeleteAsync(deleteCategoryRequest);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Null(result.Data);
+            Assert.False(result.IsSuccess);
+        }
+
+        [Fact]
+        public async Task DeleteAsync_CategoryExists_ReturnsSuccessResponse()
+        {
+            // Arrange
+            var userId = Guid.NewGuid().ToString();
+            var category = new Category
+            {
+                UserId = userId,
+                Title = "Title original",
+                Description = "Description original"
+            };
+
+            await _appDbContext.Categories.AddAsync(category);
+            await _appDbContext.SaveChangesAsync();
+
+            var deleteCategoryRequest = new DeleteCategoryRequest
+            {
+                Id = category.Id,
+                UserId = userId
+            };
+
+            // Act
+            var result = await _handler.DeleteAsync(deleteCategoryRequest);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(result.IsSuccess);
+            Assert.NotNull(result.Data);
+        }
+
+        [Fact]
+        public async Task GetByIdAsync_CategoryExists_ReturnsSuccessResponse()
+        {
+            // Arrange
+            var userId = Guid.NewGuid().ToString();
+            var category = new Category
+            {
+                UserId = userId,
+                Title = "Title original",
+                Description = "Description original"
+            };
+
+            await _appDbContext.Categories.AddAsync(category);
+            await _appDbContext.SaveChangesAsync();
+
+            var getCategoryByIdRequest = new GetCategoryByIdRequest
+            {
+                Id = category.Id,
+                UserId = userId
+            };
+
+            // Act
+            var result = await _handler.GetByIdAsync(getCategoryByIdRequest);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(result.IsSuccess);
+            Assert.NotNull(result.Data);
+            Assert.Equal(category.Title, result.Data.Title);
+            Assert.Equal(category.Description, result.Data.Description);
+        }
+
+        [Fact]
+        public async Task GetAllAsync_ReturnsSuccessResponse()
+        {
+            // Arrange
+            var userId = Guid.NewGuid().ToString();
+            var categories = new List<Category>
+            {
+                new(){ UserId = userId, Title = "Category number one" },
+                new(){ UserId = userId, Title = "Category number two" },
+                new(){ UserId = userId, Title = "Category number three" }
+            };
+
+            await _appDbContext.Categories.AddRangeAsync(categories);
+            await _appDbContext.SaveChangesAsync();
+
+            var getAllCategoriesRequest = new GetAllCategoriesRequest
+            {
+                UserId = userId
+            };
+
+            // Act
+            var result = await _handler.GetAllAsync(getAllCategoriesRequest);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(result.IsSuccess);
+            Assert.NotNull(result.Data);
+            Assert.Equal(3, result.TotalCount);
+            Assert.Equal(3, result.Data.Count);
+            Assert.Collection(result.Data,
+                category1 =>
+                {
+                    Assert.Equal("Category number one", category1.Title);
+                },
+                category2 =>
+                {
+                    Assert.Equal("Category number three", category2.Title);
+                },
+                category3 =>
+                {
+                    Assert.Equal("Category number two", category3.Title);
+                });
+        }
+    }
+}
---
+++ bugs/Dima.Api.Tests/Handlers/TransactionHandlerTests.cs
@@ -0,0 +1,288 @@
+﻿using System.Linq;
+using Dima.Api.Data;
+using Dima.Api.Handlers;
+using Dima.Core.Enums;
+using Dima.Core.Models;
+using Dima.Core.Requests.Categories;
+using Dima.Core.Requests.Transactions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Dima.Api.Tests.Handlers
+{
+    public class TransactionHandlerTests
+    {
+        private readonly DbContextOptions<AppDbContext> _options;
+        private readonly AppDbContext _appDbContext;
+        private readonly TransactionHandler _handler;
+        private string UserId = Guid.NewGuid().ToString();
+        private Category _category = null!;
+
+        public TransactionHandlerTests()
+        {
+            _options = new DbContextOptionsBuilder<AppDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+
+            _appDbContext = new AppDbContext(_options);
+            _handler = new TransactionHandler(_appDbContext);
+
+            _category = new Category
+            {
+                UserId = UserId,
+                Title = "Title original",
+                Description = "Description original"
+            };
+        }
+
+        private async Task CreateCategory()
+        {
+
+            await _appDbContext.Categories.AddAsync(_category);
+            await _appDbContext.SaveChangesAsync();
+        }
+
+        [Fact]
+        public async Task CreateAsync_ValidRequest_ReturnSuccessResponse()
+        {
+            // Arrange
+            await CreateCategory();
+
+            var createTransactionRequest = new CreateTransactionRequest
+            {
+                UserId = Guid.NewGuid().ToString(),
+                Title = "Transaction Test",
+                Amount = 1.0m,
+                Type = ETransactionType.Withdraw,
+                PaidOrReceivedAt = DateTime.Now,
+                CategoryId = _category.Id
+            };
+
+            // Act
+            var result = await _handler.CreateAsync(createTransactionRequest);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(result.IsSuccess);
+            Assert.NotNull(result.Data);
+            Assert.Equal(createTransactionRequest.UserId, result.Data.UserId);
+            Assert.Equal(createTransactionRequest.Title, result.Data.Title);
+            Assert.Equal(createTransactionRequest.Amount, result.Data.Amount);
+            Assert.Equal(createTransactionRequest.Type, result.Data.Type);
+            Assert.Equal(createTransactionRequest.PaidOrReceivedAt, result.Data.PaidOrReceivedAt);
+            Assert.Equal(createTransactionRequest.CategoryId, result.Data.CategoryId);
+        }
+
+        [Fact]
+        public async Task UpdateAsync_TransactionNotExists_ReturnsNotFoundResponse()
+        {
+            // Arrange
+            await CreateCategory();
+            var updateTransactionRequest = new UpdateTransactionRequest
+            {
+                Id = 10000,
+                UserId = Guid.NewGuid().ToString(),
+                Title = "Transaction Test",
+                Amount = 1.0m,
+                Type = ETransactionType.Withdraw,
+                PaidOrReceivedAt = DateTime.Now,
+                CategoryId = _category.Id
+            };
+
+            // Act
+            var result = await _handler.UpdateAsync(updateTransactionRequest);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Null(result.Data);
+            Assert.False(result.IsSuccess);
+        }
+
+        [Fact]
+        public async Task UpdateAsync_TranscationExists_ReturnsSuccessResponse()
+        {
+            // Arrange
+            await CreateCategory();
+            var transaction = new Transaction
+            {
+                UserId = Guid.NewGuid().ToString(),
+                Title = "Transaction original",
+                Amount = 1.0m,
+                Type = ETransactionType.Withdraw,
+                PaidOrReceivedAt = DateTime.Now.Date,
+                CategoryId = _category.Id
+            };
+
+            await _appDbContext.Transactions.AddAsync(transaction);
+            await _appDbContext.SaveChangesAsync();
+
+            var updateTransactionRequest = new UpdateTransactionRequest
+            {
+                Id = transaction.Id,
+                UserId = transaction.UserId,
+                Title = "Transaction new",
+                Amount = 10.0m,
+                Type = ETransactionType.Deposit,
+                PaidOrReceivedAt = DateTime.Now.Date,
+                CategoryId = _category.Id
+            };
+
+            // Act
+            var result = await _handler.UpdateAsync(updateTransactionRequest);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(result.IsSuccess);
+            Assert.NotNull(result.Data);
+            Assert.Equal(updateTransactionRequest.Title, result.Data.Title);
+            Assert.Equal(updateTransactionRequest.Amount, result.Data.Amount);
+            Assert.Equal(updateTransactionRequest.Type, result.Data.Type);
+            Assert.Equal(updateTransactionRequest.PaidOrReceivedAt, result.Data.PaidOrReceivedAt);
+        }
+
+        [Fact]
+        public async Task DeleteAsync_TransactionNotExists_ReturnsNotFoundResponse()
+        {
+            // Arrange
+            var deleteTransactionRequest = new DeleteTransactionRequest
+            {
+                Id = 10000,
+                UserId = Guid.NewGuid().ToString()
+            };
+
+            // Act
+            var result = await _handler.DeleteAsync(deleteTransactionRequest);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.Null(result.Data);
+            Assert.False(result.IsSuccess);
+        }
+
+        [Fact]
+        public async Task DeleteAsync_TransactionExists_ReturnsSuccessResponse()
+        {
+            // Arrange
+            await CreateCategory();
+            var transaction = new Transaction
+            {
+                UserId = Guid.NewGuid().ToString(),
+                Title = "Transaction original",
+                Amount = 1.0m,
+                Type = ETransactionType.Withdraw,
+                PaidOrReceivedAt = DateTime.Now.Date,
+                CategoryId = _category.Id
+            };
+
+            await _appDbContext.Transactions.AddAsync(transaction);
+            await _appDbContext.SaveChangesAsync();
+
+            var deleteTransactionRequest = new DeleteTransactionRequest
+            {
+                Id = transaction.Id,
+                UserId = transaction.UserId
+            };
+
+            // Act
+            var result = await _handler.DeleteAsync(deleteTransactionRequest);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(result.IsSuccess);
+            Assert.NotNull(result.Data);
+        }
+
+        [Fact]
+        public async Task GetByIdAsync_TransactionExists_ReturnsSuccessResponse()
+        {
+            // Arrange
+            await CreateCategory();
+            var transaction = new Transaction
+            {
+                UserId = Guid.NewGuid().ToString(),
+                Title = "Transaction original",
+                Amount = 1.0m,
+                Type = ETransactionType.Withdraw,
+                PaidOrReceivedAt = DateTime.Now.Date,
+                CategoryId = _category.Id
+            };
+
+            await _appDbContext.Transactions.AddAsync(transaction);
+            await _appDbContext.SaveChangesAsync();
+
+            var getTransactionByIdRequest = new GetTransactionByIdRequest
+            {
+                Id = transaction.Id,
+                UserId = transaction.UserId
+            };
+
+            // Act
+            var result = await _handler.GetByIdAsync(getTransactionByIdRequest);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(result.IsSuccess);
+            Assert.NotNull(result.Data);
+            Assert.Equal(transaction.UserId, result.Data.UserId);
+            Assert.Equal(transaction.Title, result.Data.Title);
+            Assert.Equal(transaction.Amount, result.Data.Amount);
+            Assert.Equal(transaction.Type, result.Data.Type);
+            Assert.Equal(transaction.PaidOrReceivedAt, result.Data.PaidOrReceivedAt);
+            Assert.Equal(transaction.CategoryId, result.Data.CategoryId);
+        }
+
+        [Fact]
+        public async Task GetPerioddAsync_TransactionExists_ReturnsSuccessResponse()
+        {
+            // Arrange
+            await CreateCategory();
+
+            var userId = Guid.NewGuid().ToString();
+            var transactions = new List<Transaction>
+            {
+                new() {
+                    UserId = userId,
+                    Title = "Transaction filtered",
+                    Amount = 1.0m,
+                    Type = ETransactionType.Withdraw,
+                    PaidOrReceivedAt = DateTime.Now.AddDays(-1).Date,
+                    CategoryId = _category.Id
+                },
+                new() {
+                    UserId = userId,
+                    Title = "Transaction not filtered",
+                    Amount = 5.0m,
+                    Type = ETransactionType.Withdraw,
+                    PaidOrReceivedAt = DateTime.Now.AddDays(-3).Date,
+                    CategoryId = _category.Id
+                }
+            };
+
+            await _appDbContext.Transactions.AddRangeAsync(transactions);
+            await _appDbContext.SaveChangesAsync();
+
+            var getTransactionByPeriodRequest = new GetTransactionsByPeriodRequest
+            {
+                StartDate = DateTime.Now.AddDays(-2).Date,
+                EndDate = DateTime.Now.Date
+            };
+
+            // Act
+            var result = await _handler.GetByPeriodAsync(getTransactionByPeriodRequest);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.True(result.IsSuccess);
+            Assert.NotNull(result.Data);
+
+            foreach(var item in result.Data)
+            {
+                Assert.Equal(item.UserId, transactions.First().UserId);
+                Assert.Equal(item.Title, transactions.First().Title);
+                Assert.Equal(item.Amount, transactions.First().Amount);
+                Assert.Equal(item.Type, transactions.First().Type);
+                Assert.Equal(item.PaidOrReceivedAt, transactions.First().PaidOrReceivedAt);
+                Assert.Equal(item.CategoryId, transactions.First().CategoryId);
+            }
+        }
+    }
+}
