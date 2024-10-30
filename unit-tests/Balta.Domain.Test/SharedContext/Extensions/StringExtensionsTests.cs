fa94110d07cb7a271c8827ac751b3be46c79c219
Filipe Frigini <filipe.frigini@gmail.com>
2024-10-30 20:44:42 -0300

    ultimo teste para string extensions

--- unit-tests/Balta.Domain.Test/SharedContext/Extensions/StringExtensionsTests.cs
+++ unit-tests/Balta.Domain.Test/SharedContext/Extensions/StringExtensionsTests.cs
@@ -1,3 +1,4 @@
+using System.Text;
 using Balta.Domain.SharedContext.Extensions;
 
 namespace Balta.Domain.Test.SharedContext.Extensions;
@@ -5,5 +6,17 @@ namespace Balta.Domain.Test.SharedContext.Extensions;
 public class StringExtensionsTests
 {
     [Fact]
-    public void ShouldGenerateBase64FromString() => Assert.Fail();
+    public void ShouldGenerateBase64FromString()
+    {
+        // Arrange
+        string example = "example";
+        string expectedBase64 = Convert.ToBase64String(Encoding.ASCII.GetBytes(example));
+
+        // Act
+        string actualBase64 = example.ToBase64();
+
+        // Assert
+        Assert.Equal(expectedBase64, actualBase64);
+    }
+
 }
